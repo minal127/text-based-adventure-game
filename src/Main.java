f0a41d30c73fea73f0afcbb2df639382be7f71a6
Tom Fuller <tom80f@gmail.com>
2022-11-28 19:01:11 +0000

    added main loop with options TAKE, DROP and so on, also an item class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,53 @@
+package adventurereal;
+
 import java.util.Scanner;
 
 public class Main {
+    static Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to the text based adventure game");
-        Scanner keyboard = new Scanner(System.in);
+
+        Room room = new Room();
+
         System.out.println("Enter your name");
         String name = keyboard.nextLine();
         Person person = new Person(name);
         System.out.println(person.toString());
-        Room room = new Room();
+
         System.out.println(name + " you are in: " + room.generateRandomRoom());
+
+
+        while (true) {ask(room);}
+
     }
+
+    public static void ask(Room room) {
+        System.out.println("Type what you would like to do");
+        String ask = keyboard.nextLine();
+        if (ask.equals("LOOK")) {
+            System.out.println("this room is...............");
+        }
+        else if (ask.startsWith("LOOK ")) {
+            System.out.println("this object is...............");
+        }
+        else if (ask.equals("MOVE")) {
+            System.out.println("moving...............");
+        }
+        else if (ask.equals("TAKE")) {
+            System.out.println("taking..............");
+            room.generateItems();
+            System.out.println(room.items);
+            // user gets a list of items
+            // user can select number 1, 2, 3
+        }
+        else if (ask.equals("DROP")) {
+            System.out.println("whoops...............");
+        }
+        else if (ask.equals("OTHER")) {
+            System.out.println("other...............");
+        }
+
+    }
+
 }
--- src/Room.java
+++ src/Room.java
@@ -1,11 +1,27 @@
+package adventurereal;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Room {
-    String[] roomNames = {"Living Room", "Kitchen", "Main Room", "Bathroom"};
+    ArrayList<String> roomnames =  new ArrayList<>(Arrays.asList("Living Room", "Kitchen", "Main Room", "Bathroom"));
+    ArrayList<Item> items =  new ArrayList<>();
 
     public String generateRandomRoom() {
         Random generator = new Random();
-        int randomIndex = generator.nextInt(roomNames.length);
-        return roomNames[randomIndex];
+        int randomIndex = generator.nextInt(roomnames.size());
+
+        String choice = roomnames.get(randomIndex);
+        roomnames.remove(choice);
+        System.out.println(roomnames);
+        return choice;
+
+    }
+
+    public void generateItems() {
+        items.add(new Item("TV Remote", "Black", "Change Channels"));
+        items.add(new Item("Dog", "Brown", "Animal"));
+        items.add(new Item("Car Keys", "Black", "Car Keys"));
     }
 }
